8304f0b60fe68884ea95c65c24570991110bff87
Che <43485962+c-git@users.noreply.github.com>
2023-06-11 00:53:23 -0400

    Change return types to anyhow

--- src/core/write_file.rs
+++ src/core/write_file.rs
@@ -1,20 +1,19 @@
 use convert_case::{Case, Casing};
 use std::{
-    error::Error,
     fs::{remove_file, OpenOptions},
     io::Write,
     path::PathBuf,
     process::Command,
 };
 
-fn update_lib(slug_snake: &str) -> Result<(), Box<dyn Error>> {
+fn update_lib(slug_snake: &str) -> anyhow::Result<()> {
     let lib_path = PathBuf::from(format!("{}/../src/lib.rs", env!("CARGO_MANIFEST_DIR")));
     let mut lib = OpenOptions::new().append(true).open(lib_path)?;
     let _ = lib.write(format!("pub mod {slug_snake};").as_bytes())?;
     Ok(())
 }
 
-pub fn write_file(title_slug: &str, code_snippet: String) -> Result<(), Box<dyn Error>> {
+pub fn write_file(title_slug: &str, code_snippet: String) -> anyhow::Result<()> {
     let slug_snake = title_slug.to_case(Case::Snake);
     let path = PathBuf::from(format!(
         "{}/../src/{slug_snake}.rs",
