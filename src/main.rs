d69d5a4b41665be3bca36b2f0f8c832e4d6ea717
Che <43485962+c-git@users.noreply.github.com>
2023-06-10 22:08:21 -0400

    Change return type to anyhow::Result
    Simplify return types for fallible functions

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,5 @@
 use std::error::Error;
 
-use cargo_leet::{code_snippet, daily_challenge, write_file};
-
-fn main() -> Result<(), Box<dyn Error>> {
-    let mut args = std::env::args();
-    let title_slug = if args.len() == 1 {
-        daily_challenge::get_daily_challenge_slug()
-    } else if args.len() != 2 {
-        return Err("Usage: binary SLUG".into());
-    } else {
-        args.nth(1).unwrap()
-    };
-    let code_snippet = code_snippet::generate_code_snippet(&title_slug);
-    write_file::write_file(&title_slug, code_snippet)?;
+fn main() -> anyhow::Result<()> {
     Ok(())
 }
