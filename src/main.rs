385ab3b558274249cf1798d55a1e6fa4fd50215c
Che <43485962+c-git@users.noreply.github.com>
2023-06-11 03:26:43 -0400

    Update CLI to work with cargo properly
    Based on example
    https://docs.rs/clap/latest/clap/_derive/_cookbook/cargo_example_derive/

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use cargo_leet::{init_logging, run, Cli};
+use cargo_leet::{init_logging, run, CargoCli};
 use clap::Parser;
 
 fn main() -> anyhow::Result<()> {
-    let cli = Cli::parse();
+    let CargoCli::Leet(cli) = CargoCli::parse();
     init_logging(cli.log_level.into())?;
     run(&cli)
 }
