3dd88e6946cee0844d321778ed9939ee45c6c2fc
Che <43485962+c-git@users.noreply.github.com>
2023-06-11 23:39:44 -0400

    Plug in replacement logger

--- src/log.rs
+++ src/log.rs
@@ -1,5 +1,7 @@
+use env_logger::Builder;
 use log::LevelFilter;
 
-pub fn init_logging(_log_level: LevelFilter) -> anyhow::Result<()> {
+pub fn init_logging(level: LevelFilter) -> anyhow::Result<()> {
+    Builder::new().filter(None, level).try_init()?;
     Ok(())
 }
