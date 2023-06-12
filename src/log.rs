8844795d7d4bd330df838e2de8dec8db539842b3
Che <43485962+c-git@users.noreply.github.com>
2023-06-11 22:44:02 -0400

    Pull out log4rs
    Causing issue in advisory checking
    error[vulnerability]: Potential segfault in the time crate
    time v0.1.45
         └── chrono v0.4.26
             └── log4rs v1.2.0
                 └── cargo-leet v0.1.0

--- src/log.rs
+++ src/log.rs
@@ -1,19 +1,5 @@
-use anyhow::Context;
 use log::LevelFilter;
-use log4rs::{
-    append::console::ConsoleAppender,
-    config::{Appender, Root},
-    encode::pattern::PatternEncoder,
-};
 
-pub fn init_logging(log_level: LevelFilter) -> anyhow::Result<()> {
-    let stdout = ConsoleAppender::builder()
-        .encoder(Box::new(PatternEncoder::new("{h({M} {l} - {m})}{n}")))
-        .build();
-
-    let config = log4rs::Config::builder()
-        .appender(Appender::builder().build("stdout", Box::new(stdout)))
-        .build(Root::builder().appender("stdout").build(log_level))?;
-    log4rs::init_config(config).context("Failed to initialize logging")?;
+pub fn init_logging(_log_level: LevelFilter) -> anyhow::Result<()> {
     Ok(())
 }
