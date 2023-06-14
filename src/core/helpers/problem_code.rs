4561ac09b25a5c1ad9f64491039790102faeabbd
Che <43485962+c-git@users.noreply.github.com>
2023-06-14 19:28:58 -0400

    Stub out main functionality for test cases

--- src/core/helpers/problem_code.rs
+++ src/core/helpers/problem_code.rs
@@ -24,4 +24,49 @@ impl ProblemCode {
     pub fn is_design(&self) -> bool {
         !self.code.starts_with("impl Solution {")
     }
+
+    pub fn get_fn_info(&self) -> anyhow::Result<FunctionInfo> {
+        todo!()
+    }
+}
+
+pub struct FunctionInfo {
+    pub name: String,
+    pub args: FunctionArgs,
+    pub return_type: Option<FunctionArgType>,
+}
+
+impl FunctionInfo {
+    pub fn get_args_with_case(&self) -> anyhow::Result<String> {
+        todo!()
+    }
+
+    pub fn get_args_names(&self) -> anyhow::Result<String> {
+        todo!()
+    }
+
+    pub(crate) fn get_test_case(&self, raw_str: &str) -> String {
+        todo!()
+    }
+}
+
+pub struct FunctionArgs {
+    raw_str: String,
+    pub args: Vec<FunctionArgType>,
+}
+
+impl FunctionArgs {
+    pub fn new(raw_string: String) -> Self {
+        todo!()
+    }
+}
+
+/// Function Arg Type (FAT)
+pub enum FunctionArgType {
+    FATi32,
+    FATVeci32,
+    FATVecVeci32,
+    FATString,
+    FATList,
+    FATTree,
 }
