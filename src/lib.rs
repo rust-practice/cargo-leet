75c51323133243c357ece8eda054a3f2a988b633
sak96 <26397224+sak96@users.noreply.github.com>
2023-06-28 22:06:10 +0530

    Merge binary and library and change default feature.

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,15 @@
 #![forbid(unsafe_code)]
 
-#[cfg(feature = "leet_env")]
-mod leetcode_env;
-#[cfg(feature = "tool")]
-mod tool;
-
 // For use in external code
 #[cfg(feature = "leet_env")]
-pub use leetcode_env::list::ListHead;
+mod leetcode_env;
 #[cfg(feature = "leet_env")]
-pub use leetcode_env::list::ListNode;
-#[cfg(feature = "leet_env")]
-pub use leetcode_env::tree::TreeNode;
-#[cfg(feature = "leet_env")]
-pub use leetcode_env::tree::TreeRoot;
+pub use leetcode_env::{
+    list::{ListHead, ListNode},
+    tree::{TreeNode, TreeRoot},
+};
 
-// For use in main.rs
 #[cfg(feature = "tool")]
-pub use crate::tool::cli::CargoCli;
+mod tool;
 #[cfg(feature = "tool")]
-pub use crate::tool::core::run;
-#[cfg(feature = "tool")]
-pub use crate::tool::log::init_logging;
+pub use crate::tool::{cli::CargoCli, core::run, log::init_logging};
