82ea03c933784c6416b58a6b6e4fbbad1c7eb037
One <43485962+c-git@users.noreply.github.com>
2023-06-30 20:18:03 -0400

    Allow "." to match new line
    Needed if function signature goes beyond one line

--- src/tool/core/helpers/problem_code.rs
+++ src/tool/core/helpers/problem_code.rs
@@ -52,7 +52,7 @@ impl ProblemCode {
 
     // TODO: Test extracting arguments (include all argument types)
     fn get_fn_info(code: &str) -> anyhow::Result<FunctionInfo> {
-        let re = Regex::new(r#"\n\s*pub fn ([a-z_0-9]*)\((.*)\)(?: ?-> ?(.*))? \{"#)?;
+        let re = Regex::new(r#"(?s)\n\s*pub fn ([a-z_0-9]*)\((.*)\)(?: ?-> ?(.*))? \{"#)?;
         let caps = if let Some(caps) = re.captures(code) {
             caps
         } else {
