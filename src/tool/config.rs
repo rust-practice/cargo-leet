cfabdc8c8c390fe0bff9c326279ce593f30ced65
One <43485962+c-git@users.noreply.github.com>
2023-07-04 22:41:38 -0400

    Ensure URLs end in "/"

--- src/tool/config.rs
+++ src/tool/config.rs
@@ -1,8 +1,22 @@
 pub(crate) struct Config {}
 
 impl Config {
-    // TODO: Add tests on URLs to ensure they end in the trailing "/" as this is
-    // assumed in the code using them URLs Must include trailing "/"
+    // assumed in the code using them URLs Must end with trailing "/"
     pub(crate) const LEETCODE_PROBLEM_URL: &str = "https://leetcode.com/problems/";
     pub(crate) const LEETCODE_GRAPH_QL: &str = "https://leetcode.com/graphql/";
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn problem_url_ends_with_slash() {
+        assert!(Config::LEETCODE_PROBLEM_URL.ends_with('/'));
+    }
+
+    #[test]
+    fn graph_ql_url_ends_with_slash() {
+        assert!(Config::LEETCODE_GRAPH_QL.ends_with('/'));
+    }
 }
