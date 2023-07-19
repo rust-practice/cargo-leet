11484adc00be60ee3fe035539a1f1841d9b9cbd5
One <43485962+c-git@users.noreply.github.com>
2023-07-19 00:42:04 -0400

    Add template to link

--- src/tool/core/helpers/problem_code.rs
+++ src/tool/core/helpers/problem_code.rs
@@ -369,7 +369,7 @@ impl TryFrom<&str> for FunctionArgType {
             "Option<Box<ListNode>>" => List,
             "Option<Rc<RefCell<TreeNode>>>" => Tree,
             trimmed_value => {
-                warn!("Unknown type {trimmed_value:?} found please report this in an issue https://github.com/rust-practice/cargo-leet/issues/");
+                warn!("Unknown type {trimmed_value:?} found please report this in an issue https://github.com/rust-practice/cargo-leet/issues/new?&labels=bug&template=missing_type.md");
                 Other {
                     raw: trimmed_value.to_string(),
                 }
