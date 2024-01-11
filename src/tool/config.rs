57a8bbb5b8ef201fe01287f983f3382a419681d2
One <43485962+c-git@users.noreply.github.com>
2024-01-11 15:49:25 +0000

    FIx lints from new version of rust

--- src/tool/config.rs
+++ src/tool/config.rs
@@ -2,8 +2,8 @@ pub(crate) struct Config {}
 
 impl Config {
     // assumed in the code using them URLs Must end with trailing "/"
-    pub(crate) const LEETCODE_PROBLEM_URL: &str = "https://leetcode.com/problems/";
-    pub(crate) const LEETCODE_GRAPH_QL: &str = "https://leetcode.com/graphql/";
+    pub(crate) const LEETCODE_PROBLEM_URL: &'static str = "https://leetcode.com/problems/";
+    pub(crate) const LEETCODE_GRAPH_QL: &'static str = "https://leetcode.com/graphql/";
 }
 
 #[cfg(test)]
