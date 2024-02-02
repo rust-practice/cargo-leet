f4fcd059d3c580ab35944251d496a6b1cebc2318
One <43485962+c-git@users.noreply.github.com>
2024-02-02 09:58:20 -0500

    chore: add possible solution if script fails

--- scripts/hooks_setup.bat
+++ scripts/hooks_setup.bat
@@ -1,3 +1,6 @@
+@echo off
 @REM Expected to be run from the scripts folder
 
 copy commit-msg ..\.git\hooks\
+
+if %ERRORLEVEL% NEQ 0 echo "[error] The copy seems to have failed. This script expects to be run from inside the scripts folder"
--- scripts/hooks_setup.sh
+++ scripts/hooks_setup.sh
@@ -3,3 +3,9 @@
 # Expected to be run from the scripts folder
 
 cp commit-msg ../.git/hooks/
+
+if [ $? != 0 ]
+then
+    echo "[error] The copy seems to have failed. This script expects to be run from inside the scripts folder"
+    exit 1
+fi
