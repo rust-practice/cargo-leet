a0f6e18d084710b6799cdf44dbaf154eb78709c1
One <43485962+c-git@users.noreply.github.com>
2024-02-02 10:01:24 -0500

    chore: remove quotes from windows bat file for echo
    The quotes were showing in the message

--- scripts/hooks_setup.bat
+++ scripts/hooks_setup.bat
@@ -3,4 +3,4 @@
 
 copy commit-msg ..\.git\hooks\
 
-if %ERRORLEVEL% NEQ 0 echo "[error] The copy seems to have failed. This script expects to be run from inside the scripts folder"
+if %ERRORLEVEL% NEQ 0 echo [error] The copy seems to have failed. This script expects to be run from inside the scripts folder
