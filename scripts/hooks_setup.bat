321271243f4d92e956cd05844dd553e961636fae
One <43485962+c-git@users.noreply.github.com>
2024-02-01 23:13:01 -0500

    chore: add scripts to help setup recommended hooks
    Provide script for users to add a git hook to
    ensure that commit message match preferred format

---
+++ scripts/hooks_setup.bat
@@ -0,0 +1,3 @@
+@REM Expected to be run from the scripts folder
+
+copy commit-msg ..\.git\hooks\
---
+++ scripts/hooks_setup.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# Expected to be run from the scripts folder
+
+cp commit-msg ../.git/hooks/
