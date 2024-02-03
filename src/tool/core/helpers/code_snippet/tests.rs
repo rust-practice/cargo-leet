614c9b5286bfac0160b9b4a4f5f44941c6275c7a
One <43485962+c-git@users.noreply.github.com>
2024-02-03 00:11:55 -0500

    test: save command and add progress update

--- src/tool/core/helpers/code_snippet/tests.rs
+++ src/tool/core/helpers/code_snippet/tests.rs
@@ -12,7 +12,12 @@ use crate::tool::core::helpers::{
 #[ignore = "Only use for downloading responses"]
 fn download_response_from_leetcode(title_slugs: SlugList) {
     for title_slug in title_slugs {
-        std::thread::sleep(std::time::Duration::from_millis(get_rnd_request_delay()));
+        let sleep_delay = std::time::Duration::from_millis(get_rnd_request_delay());
+        println!(
+            "Going to sleep for {} milliseconds before requesting and trying to save {title_slug}",
+            sleep_delay.as_millis()
+        );
+        std::thread::sleep(sleep_delay); // Sleep to not go too hard on leetcode API
         let response_string = external_request_code_snippet(title_slug).unwrap();
         let path = super::path_local_store_code_snippet(title_slug);
         let mut file = std::fs::OpenOptions::new()
@@ -23,7 +28,9 @@ fn download_response_from_leetcode(title_slugs: SlugList) {
             .with_context(|| format!("failed to save response to {path:?}"))
             .unwrap();
         file.write_all(response_string.as_bytes()).unwrap();
+        println!("Save of {title_slug:?} completed\n");
     }
+    println!("Successfully saved all responses");
 }
 
 #[rstest]
