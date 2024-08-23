25b7ee1abbf82c08681bdfb95b5b9fe49600bc50
Anton Aparin <anton@antonaparin.com>
2024-08-24 00:59:14 +0200

    Add active and test subcommands

---
+++ src/tool/core/active.rs
@@ -0,0 +1,33 @@
+use std::path::Path;
+
+use anyhow::{bail, Context};
+
+use crate::tool::{cli, config_file::ConfigFile};
+
+pub(crate) fn do_active(args: &cli::ActiveArgs) -> anyhow::Result<()> {
+    let mut config = ConfigFile::load().unwrap_or_default();
+    match &args.problem_slug {
+        Some(slug) => {
+            if !Path::new(format!("src/{slug}.rs").as_str()).exists() {
+                bail!("problem {slug} does not exist");
+            }
+            if slug == "lib.rs" {
+                bail!("cannot set active problem to lib.rs");
+            }
+
+            config.active = Some(slug.to_string());
+            config.save().context("failed to save config")?;
+            println!("Set active problem to {slug}");
+        }
+        None => match &config.active {
+            Some(active) => {
+                println!("Active problem: {active}");
+            }
+            None => {
+                println!("No active problem set");
+            }
+        },
+    }
+
+    Ok(())
+}
